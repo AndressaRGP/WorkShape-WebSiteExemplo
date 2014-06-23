474be027ee6e0bbc17b36f1cdf044c1fc24218ca
AndressaRGP <Debeka123>
2014-06-23 15:23:07 -0400

    ffe

--- scripts/facebookLogin.js
+++ scripts/facebookLogin.js
@@ -102,7 +102,7 @@ function loginWorkshape(fbUID){
                                 window.location.href='createTeam.html'
                                 }
                                 });
-                         });
+                         
 
 
 
