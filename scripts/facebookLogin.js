729fb748281a03c6566fc6b8af2346e873958836
AndressaRGP <Debeka123>
2014-06-24 11:31:34 -0400

    added dataType post request
    dataType:Json

--- scripts/facebookLogin.js
+++ scripts/facebookLogin.js
@@ -97,6 +97,7 @@ function loginWorkshape(faceUID){
                          $.ajax({
                                 type: "POST",
                                 url: "http://192.168.0.12:9000/users",
+                                dataType:'json',
                                 data:{ fbUID : faceUID},  // falar o que esta enviando
                                 success: function(msg) {
                                   $.cookie("sessionID",msg.sessionID)  
