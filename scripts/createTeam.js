169c425331a4be3de6578424abbc5794fea08730
AndressaRGP <Debeka123>
2014-06-24 10:01:37 -0400

    Changed createTeam file and script createTeam

--- scripts/createTeam.js
+++ scripts/createTeam.js
@@ -1,13 +1,13 @@
-function checkInput(){
+function createTeam(){
     //Get value of input field with id ="numb"
     var teamN= document.getElementById("teamName").value;
-    var teamEmail =document.getElementById("email").value; //maybe not, tae it off.
-    var adminID = document.getElementById("adminID").value;
+   // var teamEmail =document.getElementById("email").value; //maybe not, tae it off.
+    //var adminID = document.getElementById("adminID").value;
     
     //Get the element with id = "demo"
     
     //If value is apace or not a number
-    if((teamN.trim() == "") || teamEmail.trim()== "" || adminID.trim()== "")
+    if((teamN.trim() == ""))
     {
         alert("You did not fill the team Name  or email, Please enter with a name");
     
