cab0814e3999922a4a8f0e44732fad9a5b940d40
AndressaRGP <Debeka123>
2014-08-11 10:51:12 -0400

    Changing form from serialize to keys

--- scripts/createTeam.js
+++ scripts/createTeam.js
@@ -1,22 +1,22 @@
 function createTeam(){
     //Get value of input field with id ="numb"
-    var teamN= document.getElementById("teamName").value;
-    var companyName =document.getElementById("companyName").value; //maybe not, tae it off.
-    var charityName = document.getElementById("charityName").value;
-    //Get the element with id = "demo"
+    var _teamName= document.getElementById("teamName").value;
+    var _companyName =document.getElementById("companyName").value; //maybe not, tae it off.
+    var _charityID = document.getElementById("charityName").value;
+    var _teamDescription = document.getElementById("teamDescription").value;
     
     //If value is apace or not a number
     if((teamN.trim() === "") || (companyName.trim() === "") || (charityName.trim() === ""))
     {
         alert("You did not fill the team Name  or companyName, Please enter with a name");
     
-    }else{
+    }else{//('#formName').serialize(), not working anyways 
              var sessionID = $.cookie("sessionID")
              $.ajax({
                     type: "POST",
                     url: "http://192.168.1.147:9000/team/?sessionID="+sessionID,
                     dataType:'json',
-                    data: $("#create_team_form").serialize(),
+                    data:{teamName: _teamName,companyName:_companyName,teamDescription:_teamDescription,charityID:_charityID,},
                     success: function(msg) {
                         alert("team supposedly saved")
                         $.cookie("teamID",msg.teamID)
