9290133c87f9f0c6892cfe200de204334d2b3758
AndressaRGP <Debeka123>
2014-08-11 13:52:11 -0400

    Added id's to menus and animation to leaderboard

---
+++ scripts/leaderboard.js
@@ -0,0 +1,6 @@
+var open = true
+$(".createMenu").click(function () {
+  $(".team").stop().animate({top: open ? -1000 : 70}, function() {
+                              open = !open;
+                             })
+})
