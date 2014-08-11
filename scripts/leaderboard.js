ca8df6d8842199f31c7d7e66d5ff997e02dde49a
AndressaRGP <Debeka123>
2014-08-11 13:56:50 -0400

    changing the leader.js

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -1,5 +1,5 @@
 var open = true
-$(".createMenu").click(function () {
+$("#createMenu").click(function () {
   $(".team").stop().animate({top: open ? -1000 : 70}, function() {
                               open = !open;
                              })
