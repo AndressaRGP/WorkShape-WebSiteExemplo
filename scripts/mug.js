7218d1f310d251b9bb6d21d3611910633e869701
AndressaRGP <Debeka123>
2014-08-12 15:12:05 -0400

    fixing background

--- scripts/mug.js
+++ scripts/mug.js
@@ -1,3 +1,12 @@
 $(".menu-opener").click(function(){
   $(".menu-opener, .menu-opener-inner, .menu").toggleClass("active");
 });
+
+var teamContainerOpen = false
+$("#createMenu").click(function () {
+	$(".team").stop().animate({
+		top: teamContainerOpen ? -1000 : 0
+	}, function() {
+		teamContainerOpen = !teamContainerOpen;
+	})
+})
